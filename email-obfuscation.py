d7aa2fc697322a6784bd2ad8139482b923ce74b3
Lionel Chanson <lionel@soreia.com>
2014-07-01 15:33:08 +0200

    write argparse command tool

---
+++ email-obfuscation.py
@@ -0,0 +1,11 @@
+#!/bin/env python
+
+import argparse
+
+parser = argparse.ArgumentParser(
+            description='Generate a string to obfuscate your email address')
+parser.add_argument('email', type=str, help='email address to obfuscate')
+
+args = parser.parse.args()
+
+print(args.email)
