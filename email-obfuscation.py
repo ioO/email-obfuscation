605fb2cd6191fba44193641c3caeb70b8b177838
Lionel Chanson <lionel@soreia.com>
2014-07-01 15:46:05 +0200

    lambda function to get html ascii entity

--- email-obfuscation.py
+++ email-obfuscation.py
@@ -11,4 +11,10 @@ args = parser.parse_args()
 # get args.email and transform char in list of ascii code
 ascii_char_list = list(ord(char) for char in list(args.email))
 
+# lambda function to transform ascii to html ascii entity
+# randomly keep ascii in decimal or return ascii hex
+
+transform = lambda s: random.choice(
+                ('&#'+str(s)+';', str(hex(s)).replace('0', '&#', 1)+';'))
+
 print(ascii_char_list)
