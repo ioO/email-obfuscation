3b0d0ba342f0c2bd907ec2b3b5067619703a8ece
Lionel Chanson <lionel@soreia.com>
2014-07-01 18:09:40 +0200

    generate a link with mailto

--- email-obfuscation.py
+++ email-obfuscation.py
@@ -17,9 +17,15 @@ ascii_char_list = list(ord(char) for char in list(args.email))
 
 # lambda function to transform ascii to html ascii entity
 # randomly keep ascii in decimal or return ascii hex
-
 transform = lambda s: random.choice(
                 ('&#'+str(s)+';', str(hex(s)).replace('0', '&#', 1)+';'))
 
 #print a string of html entities
+print('Email address obfuscated:')
 print(''.join(list(map(transform, ascii_char_list))))
+
+if(args.mailto):
+    link_mailto = 'mailto:'+args.email
+    ascii_char_mailto = list(ord(char) for char in list(link_mailto))
+    print('mailto link obfuscated :')
+    print(''.join(list(map(transform, ascii_char_mailto))))
